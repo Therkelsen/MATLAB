8162f7aa843ece711c5319524a3b588ec5950d27
Therkelsen <37107686+Therkelsen@users.noreply.github.com>
2022-02-18 15:33:11 +0100

    Reorganization

---
+++ MATLAB Files/Control Engineering/Lecture 3 - DC Motor/dcMotorPar.m
@@ -0,0 +1,12 @@
+clc
+clear
+close all
+
+%% Parameters
+J = 0.181;      % [kgm^2]
+kf = 0.1;       % [Nm/(rad/s)]
+kb = 36.9e-3;   % [V/(rad/s)]
+km = 20;        % [Nm/A]
+R = 0.608;      % [ohm]
+L = 0.463e-3;   % [H]
+%%
